a2a05c8a4f384220eef71630b1aad34cccb0fd23
Gabriel Altay <gabriel.altay@gmail.com>
2023-09-22 03:27:57 -0400

    rearrange model names

--- shellm/main.py
+++ shellm/main.py
@@ -12,20 +12,20 @@ embedding_models = [
 
 completion_models = [
     'gpt-3.5-turbo-instruct',
+    'gpt-3.5-turbo-instruct-0914',
     'davinci-002',
     'babbage-002',
 ]
 
 chat_completion_models = [
-    'gpt-4-0613',
     'gpt-4',
-    'gpt-3.5-turbo-instruct-0914',
+    'gpt-4-0613',
+    'gpt-4-0314',
+    'gpt-3.5-turbo-16k',
+    'gpt-3.5-turbo-16k-0613',
+    'gpt-3.5-turbo',
     'gpt-3.5-turbo-0613',
     'gpt-3.5-turbo-0301',
-    'gpt-3.5-turbo-16k',
-    'gpt-3.5-turbo',
-    'gpt-4-0314',
-    'gpt-3.5-turbo-16k-0613',
 ]
 
 
